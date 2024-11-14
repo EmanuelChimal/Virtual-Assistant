f7999d746e12ed96a6da232742e1217b30a02f83
Emanuel <armendarizchimalemanuel@gmail.com>
2024-11-13 18:05:40 -0600

    info metereologica

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+import requests
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
@@ -13,3 +14,18 @@ def handle_common_questions(question):
         # Agrega más preguntas/respuestas aquí.
     }
     return responses.get(question.lower(), "Lo siento, no tengo una respuesta para eso.")
+
+
+
+def get_weather(city):
+    api_key = 'aea42fe6022b3e9064b8e3cc9542eb25'
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    
+    if response.status_code == 200:
+        weather = data['weather'][0]['description']
+        temp = data['main']['temp']
+        return f"El clima en {city} es {weather} con una temperatura de {temp}°C."
+    else:
+        return "No se pudo obtener la información del clima. Verifica la ciudad."
